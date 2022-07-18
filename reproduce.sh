35864c1b2188b5b7bd1f880778e0d0e4dd68c9e9
Max Fan <root@max.fan>
2022-07-18 18:19:52 -0400

    Fix bug with year variable

--- reproduce.sh
+++ reproduce.sh
@@ -6,10 +6,10 @@ python filter_cbsas.py
 # Subsection tracts
 fd "shp" cbsas/defs/ -x python intersection.py processed/2020_tracts.shp {} cbsas/2020/{/.}_cbsa_tracts.shp
 fd "shp" cbsas/defs/ -x python intersection.py processed/2010_tracts.shp {} cbsas/2010/{/.}_cbsa_tracts.shp
-fd "shp" cbsas/defs/ -x python intersection.py processed/2010_tracts.shp {} cbsas/2000/{/.}_cbsa_tracts.shp
-fd "shp" cbsas/defs/ -x python intersection.py processed/2010_tracts.shp {} cbsas/1990/{/.}_cbsa_tracts.shp
-fd "shp" cbsas/defs/ -x python intersection.py processed/2010_tracts.shp {} cbsas/1980/{/.}_cbsa_tracts.shp
-fd "shp" cbsas/defs/ -x python intersection.py processed/2010_tracts.shp {} cbsas/1970/{/.}_cbsa_tracts.shp
+fd "shp" cbsas/defs/ -x python intersection.py processed/2000_tracts.shp {} cbsas/2000/{/.}_cbsa_tracts.shp
+fd "shp" cbsas/defs/ -x python intersection.py processed/1990_tracts.shp {} cbsas/1990/{/.}_cbsa_tracts.shp
+fd "shp" cbsas/defs/ -x python intersection.py processed/1980_tracts.shp {} cbsas/1980/{/.}_cbsa_tracts.shp
+fd "shp" cbsas/defs/ -x python intersection.py processed/1970_tracts.shp {} cbsas/1970/{/.}_cbsa_tracts.shp
 
 # Generate dual graphs
 fd _cbsa_tracts.shp cbsas/ -x python3 gen-duals.py {} {.}_orig.json {.}_connected.json
