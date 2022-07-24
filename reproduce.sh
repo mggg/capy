d37e36986460e8a9776a96e4a14b44a134daafa2
Max Fan <root@max.fan>
2022-07-24 16:47:55 -0400

    Parallelize cbsa filtering step

--- reproduce.sh
+++ reproduce.sh
@@ -6,12 +6,7 @@ set -euxo pipefail
 python scripts/preprocess.py
 
 # Generate different dual graphs based on the various CBSA defs
-python scripts/filter_cbsas.py --filename configs/list1_february_2013.xls --definition-tracts processed/2010_tracts.shp
-python scripts/filter_cbsas.py --filename configs/list1_july_2015.xls --definition-tracts processed/2010_tracts.shp
-python scripts/filter_cbsas.py --filename configs/list1_august_2017.xls --definition-tracts processed/2010_tracts.shp
-python scripts/filter_cbsas.py --filename configs/list1_april_2018.xls --definition-tracts processed/2010_tracts.shp
-python scripts/filter_cbsas.py --filename configs/list1_september_2018.xls --definition-tracts processed/2010_tracts.shp
-python scripts/filter_cbsas.py --filename configs/list1_march_2020.xls --definition-tracts processed/2020_tracts.shp
+cat scripts/filter.sh | parallel --bar
 
 # Subsection tracts by computing intersections
 cat scripts/overlaps.sh | parallel --bar
---
+++ scripts/filter.sh
@@ -0,0 +1,6 @@
+python scripts/filter_cbsas.py --filename configs/list1_february_2013.xls --definition-tracts processed/2010_tracts.shp
+python scripts/filter_cbsas.py --filename configs/list1_july_2015.xls --definition-tracts processed/2010_tracts.shp
+python scripts/filter_cbsas.py --filename configs/list1_august_2017.xls --definition-tracts processed/2010_tracts.shp
+python scripts/filter_cbsas.py --filename configs/list1_april_2018.xls --definition-tracts processed/2010_tracts.shp
+python scripts/filter_cbsas.py --filename configs/list1_september_2018.xls --definition-tracts processed/2010_tracts.shp
+python scripts/filter_cbsas.py --filename configs/list1_march_2020.xls --definition-tracts processed/2020_tracts.shp
