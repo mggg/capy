7e5db2ae2cfcbb8376d9fbc43a2595b158aacfbd
Max Fan <root@max.fan>
2022-07-24 12:15:37 -0400

    Add overlaps.sh calc file

---
+++ overlaps.sh
@@ -0,0 +1,6 @@
+python pipeline/overlaps.py processed/2020_tracts.shp 'cbsas/defs/*.shp' cbsas/2020
+python pipeline/overlaps.py processed/2010_tracts.shp 'cbsas/defs/*.shp' cbsas/2010
+python pipeline/overlaps.py processed/2000_tracts.shp 'cbsas/defs/*.shp' cbsas/2000
+python pipeline/overlaps.py processed/1990_tracts.shp 'cbsas/defs/*.shp' cbsas/1990
+python pipeline/overlaps.py processed/1980_tracts.shp 'cbsas/defs/*.shp' cbsas/1980
+python pipeline/overlaps.py processed/1970_tracts.shp 'cbsas/defs/*.shp' cbsas/1970
