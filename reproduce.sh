f287cfee28fb4fd486b9f012fbff893cbb8f5687
Max Fan <root@max.fan>
2022-07-26 17:32:38 -0400

    Swap x_col and y_col

--- reproduce.sh
+++ reproduce.sh
@@ -18,11 +18,11 @@ cat scripts/overlaps.sh | parallel --bar
 fd _cbsa_tracts.shp cbsas/ | parallel --bar python3 pipeline/gen_duals.py {} {.}_orig.json {.}_connected.json
 
 # Calculate metrics, but first generate headers
-python pipeline/calculate_metrics.py cbsas/2020/186847_39460_march_2020_cbsa_tracts_connected.json WHITE BLACK TOTPOP --headers-only > outputs/white_black.csv
-fd connected.json cbsas/ | parallel --bar python pipeline/calculate_metrics.py {} WHITE BLACK TOTPOP >> outputs/white_black.csv
+python pipeline/calculate_metrics.py cbsas/2020/186847_39460_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only > outputs/white_black.csv
+fd connected.json cbsas/ | parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP >> outputs/white_black.csv
 
-python pipeline/calculate_metrics.py cbsas/2020/186847_39460_march_2020_cbsa_tracts_connected.json WHITE BLACK TOTPOP --headers-only > outputs/white_poc.csv
-fd connected.json cbsas/ | parallel --bar python pipeline/calculate_metrics.py {} WHITE POC TOTPOP >> outputs/white_poc.csv
+python pipeline/calculate_metrics.py cbsas/2020/186847_39460_march_2020_cbsa_tracts_connected.json POC WHITE TOTPOP --headers-only > outputs/white_poc.csv
+fd connected.json cbsas/ | parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP >> outputs/white_poc.csv
 
 # Parse CSVs and add metadata
 python scripts/parse_output.py outputs/white_black.csv outputs/white_black_parsed.csv
