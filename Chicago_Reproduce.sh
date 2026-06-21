c5145938fe045ab1211cb77b475d718645ba0271
Sam Stephenson <samstephenson@Sams-MacBook-Pro.local>
2026-06-21 11:40:49 -0500

    added 2010 data + made blocks include the wisconsin and indiana parts of metro area

--- Chicago_Reproduce.sh
+++ Chicago_Reproduce.sh
@@ -1,61 +1,119 @@
 set -euxo pipefail
 
 python pipeline/overlaps.py \
-    "Chicago_2020_Maup_Data/tracts/tracts_joined/tracts_joined.shp" \
+    "Chicago_Maup_Data/tracts/tracts_joined_2010/tracts_joined_2010.shp" \
     "cbsas/defs/9618502_16980_march_2020.shp" \
     chicago-maup \
-    --prefix "tract_"
+    --prefix "tract_2010_"
 
 python pipeline/overlaps.py \
-    "Chicago_2020_Maup_Data/blocks/blocks_joined/blocks_joined.shp" \
+    "Chicago_Maup_Data/tracts/tracts_joined_2020/tracts_joined_2020.shp" \
     "cbsas/defs/9618502_16980_march_2020.shp" \
     chicago-maup \
-    --prefix "block_"
+    --prefix "tract_2020_"
 
 python pipeline/overlaps.py \
-    "Chicago_2020_Maup_Data/block groups/blk_grps_joined/blk_grps_joined.shp" \
+    "Chicago_Maup_Data/blocks/blocks_joined_2010/blocks_joined_2010.shp" \
     "cbsas/defs/9618502_16980_march_2020.shp" \
     chicago-maup \
-    --prefix "bg_"
+    --prefix "block_2010_"
 
+python pipeline/overlaps.py \
+    "Chicago_Maup_Data/blocks/blocks_joined_2020/blocks_joined_2020.shp" \
+    "cbsas/defs/9618502_16980_march_2020.shp" \
+    chicago-maup \
+    --prefix "block_2020_"
 
-fd .shp chicago-maup/ | parallel --bar python pipeline/gen_duals.py {} {.}_orig.json {.}_connected.json --attr GEOID
+python pipeline/overlaps.py \
+    "Chicago_Maup_Data/block groups/blk_grps_joined_2010/blk_grps_joined_2010.shp" \
+    "cbsas/defs/9618502_16980_march_2020.shp" \
+    chicago-maup \
+    --prefix "bg_2010_"
+    
+python pipeline/overlaps.py \
+    "Chicago_Maup_Data/block groups/blk_grps_joined_2020/blk_grps_joined_2020.shp" \
+    "cbsas/defs/9618502_16980_march_2020.shp" \
+    chicago-maup \
+    --prefix "bg_2020_"
+
+fd -g '**.shp' chicago-maup/ \
+| parallel --bar python pipeline/gen_duals.py {} {.}_orig.json {.}_connected.json --attr GEOID
 
 (
-python pipeline/calculate_metrics.py chicago-maup/tract_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
-fd -g "tract_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+python pipeline/calculate_metrics.py chicago-maup/tract_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "tract_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
 | parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
-) > outputs/tract_white_black_chicago.csv
+) > outputs/tract_white_black_chicago_2010.csv
 
 (
-python pipeline/calculate_metrics.py chicago-maup/tract_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
-fd -g "tract_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
-| parallel --bar python pipeline/calculate_metrics.py {} BLACK POC TOTPOP
-) > outputs/tract_white_poc_chicago.csv
+python pipeline/calculate_metrics.py chicago-maup/tract_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "tract_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/tract_white_poc_chicago_2010.csv
 
 
 (
-python pipeline/calculate_metrics.py chicago-maup/bg_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
-fd -g "bg_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+python pipeline/calculate_metrics.py chicago-maup/tract_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "tract_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
 | parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
-) > outputs/bg_white_black_chicago.csv
+) > outputs/tract_white_black_chicago_2020.csv
 
 (
-python pipeline/calculate_metrics.py chicago-maup/bg_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
-fd -g "bg_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
-| parallel --bar python pipeline/calculate_metrics.py {} BLACK POC TOTPOP
-) > outputs/bg_white_poc_chicago.csv
+python pipeline/calculate_metrics.py chicago-maup/tract_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "tract_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/tract_white_poc_chicago_2020.csv
 
 
 (
-python pipeline/calculate_metrics.py chicago-maup/block_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
-fd -g "block_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+python pipeline/calculate_metrics.py chicago-maup/bg_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "bg_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
 | parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
-) > outputs/block_white_black_chicago.csv
+) > outputs/bg_white_black_chicago_2010.csv
 
 (
-python pipeline/calculate_metrics.py chicago-maup/block_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
-fd -g "block_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
-| parallel --bar python pipeline/calculate_metrics.py {} BLACK POC TOTPOP
-) > outputs/block_white_poc_chicago.csv
+python pipeline/calculate_metrics.py chicago-maup/bg_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "bg_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/bg_white_poc_chicago_2010.csv
+
+(
+python pipeline/calculate_metrics.py chicago-maup/bg_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "bg_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
+) > outputs/bg_white_black_chicago_2020.csv
+
+
+(
+python pipeline/calculate_metrics.py chicago-maup/bg_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "bg_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/bg_white_poc_chicago_2020.csv
+
+
+(
+python pipeline/calculate_metrics.py chicago-maup/block_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "block_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
+) > outputs/block_white_black_chicago_2010.csv
+
+(
+python pipeline/calculate_metrics.py chicago-maup/block_2010_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK WHITE TOTPOP --headers-only
+fd -g "block_2010_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/block_white_poc_chicago_2010.csv
+
+
+
+(
+python pipeline/calculate_metrics.py chicago-maup/block_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "block_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} BLACK WHITE TOTPOP
+) > outputs/block_white_black_chicago_2020.csv
+
+(
+python pipeline/calculate_metrics.py chicago-maup/block_2020_9618502_16980_march_2020_cbsa_tracts_connected.json BLACK POC TOTPOP --headers-only
+fd -g "block_2020_9618502_16980_march_2020_cbsa_tracts_connected.json" chicago-maup/ \
+| parallel --bar python pipeline/calculate_metrics.py {} POC WHITE TOTPOP
+) > outputs/block_white_poc_chicago_2020.csv
 
