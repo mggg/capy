c0d1e19c30f457d7acc7119a7fbc7d9c1e8cf61a
Sam Stephenson <samstephenson@Sams-MacBook-Pro.local>
2026-07-13 23:55:41 -0500

    all figures now generated

--- pipeline/generate_figures.py
+++ pipeline/generate_figures.py
@@ -95,14 +95,7 @@ METRICS = ["e_assort",
             "moran_L",
             "moran_M",
             "moran_D_1",
-            "moran_D_2"
-            "moran_A_white",	
-            "moran_P_white",	
-            "moran_L_white",
-            "moran_M_white",
-            "moran_D_1_white",
-            "moran_D_2_white"
-            ]
+            "moran_D_2"]
 
 def ensure_metadata(df: pd.DataFrame) -> pd.DataFrame:
     required = {"definition_month_year", "year", "cbsa_title", "total_population_2020"}
